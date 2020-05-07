a5689e0b0343f2919c48b3cce7513ad0defb2764
Cov Developer <shusten@hotmail.com>
2020-05-06 23:29:53 -0300

    :sparkles: add bem-vindo component
    add bem-vindo component and change background color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './compartilhado/cabecalho/cabecalho.component';
+import { BemVindoComponent } from './paginas/publico/bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    BemVindoComponent
   ],
   imports: [
     BrowserModule,
