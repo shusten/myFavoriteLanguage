bf89a1223a91f5e21c91c8244a2b43db0cf43698
Cov Developer <shusten@hotmail.com>
2020-05-07 15:11:51 -0300

    :sparkles: add cadastro screen

--- src/app/paginas/publico/cadastro/cadastro.component.ts
+++ src/app/paginas/publico/cadastro/cadastro.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Usuario } from '../../../interfaces/usuario';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent {
+  public usuario: Usuario = <Usuario>{};
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit() {
+    
   }
 
 }
