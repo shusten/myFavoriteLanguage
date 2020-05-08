c338cf3574579e2eca67f7898ea53e2263c33fec
Cov Developer <shusten@hotmail.com>
2020-05-08 01:49:17 -0300

    :recycle: refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,8 +9,7 @@ import { CabecalhoComponent } from './compartilhado/cabecalho/cabecalho.componen
 import { BemVindoComponent } from './paginas/publico/bem-vindo/bem-vindo.component';
 import { LoginComponent } from './paginas/publico/login/login.component';
 import { CadastroComponent } from './paginas/publico/cadastro/cadastro.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
