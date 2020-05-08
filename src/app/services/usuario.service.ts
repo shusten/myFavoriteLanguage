fcd24663dde64aeeadbc8f3dfd40a6f082131fd8
Cov Developer <shusten@hotmail.com>
2020-05-08 02:02:08 -0300

    :sparkles: add usuario service

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { Usuario } from '../interfaces/usuario';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private _usuario: BehaviorSubject<Usuario>;
+  public readonly usuario$: Observable<Usuario>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this._usuario = new BehaviorSubject({} as Usuario);
+    this.usuario$ = this._usuario.asObservable();
+  }
+
+  setUsuario(usuario: Usuario) {
+    this._usuario.next(usuario);
+  }
+
+  cadastrar(usuario: Usuario) {
+    const url = `${environment.linguagensApiUrl}/usuarios`;
+    return this.http.post(url, usuario);
+  }
+  
 }
