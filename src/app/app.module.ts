770c5593f35b8fc0094a79d62b316c65122f1c8e
Cov Developer <shusten@hotmail.com>
2020-05-08 17:54:52 -0300

    :construction: add token-api in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +10,7 @@ import { BemVindoComponent } from './paginas/publico/bem-vindo/bem-vindo.compone
 import { LoginComponent } from './paginas/publico/login/login.component';
 import { CadastroComponent } from './paginas/publico/cadastro/cadastro.component';
 import { DashboardComponent } from './paginas/restrito/dashboard/dashboard.component';
+import { TokenApiService } from './interceptadores/token-api.service';
 
 
 @NgModule({
@@ -27,7 +28,13 @@ import { DashboardComponent } from './paginas/restrito/dashboard/dashboard.compo
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenApiService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
