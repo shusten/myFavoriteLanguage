49e090cd5a502e77af8152edcdd28ab4dcea0a69
Cov Developer <shusten@hotmail.com>
2020-05-08 01:46:43 -0300

    :sparkles: add app service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BemVindoComponent } from './paginas/publico/bem-vindo/bem-vindo.compone
 import { LoginComponent } from './paginas/publico/login/login.component';
 import { CadastroComponent } from './paginas/publico/cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  linguagensApiUrl: 'https://deviup.com.br:3003/api'
 };
+
+export const TOKEN_STORAGE = "NOME_CHAVE_TOKEN";
 
 /*
  * For easier debugging in development mode, you can import the following file
