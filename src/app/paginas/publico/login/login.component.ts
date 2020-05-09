f58e2e4f3a406b37c014ca3973583164bad3b53e
Cov Developer <shusten@hotmail.com>
2020-05-09 17:30:47 -0300

    :sparkles: add login component

--- src/app/paginas/publico/login/login.component.ts
+++ src/app/paginas/publico/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
+
 import { Usuario } from '../../../interfaces/usuario';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +13,24 @@ import { Usuario } from '../../../interfaces/usuario';
 export class LoginComponent{
   public usuario: Usuario = <Usuario> {};
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) { }
 
   onSubmit() {
+    this.authService.logar(this.usuario).subscribe(
+      () => {
+        this.toastr.success('Login realizado com sucesso.', 'Show!');
+      },
+      (erro) => {
+          if (erro.status && erro.status === 401) {
+          this.toastr.error('E-mail e/ou senha incorretos.', 'Falha!');
+        } else {
+          this.toastr.error('Não foi possível realizar login.', 'Falha!');
+        }
+      }
+    );
 
   }
 }
