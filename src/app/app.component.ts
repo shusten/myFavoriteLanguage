7097d9d083b1c1e20d685e1cad5ba19688a8533f
Cov Developer <shusten@hotmail.com>
2020-05-09 23:40:31 -0300

    :sparkles: add cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './services/auth.service';
+import { TokenService } from './services/token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   titulo = 'My Language - Dashboard';
+
+constructor(
+  private router: Router,
+  public authService: AuthService,
+  private tokenService: TokenService
+) { }
+
+ngOnInit() { 
+
+  if (this.tokenService.token) {
+    this.authService.criarSessao(this.tokenService.token);
+  }
+
+    this.authService.autenticado$.subscribe(autenticado => {
+      if (autenticado) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.router.navigate(['']);
+      }
+    });
+}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,14 @@
 
 export const environment = {
   production: false,
-  linguagensApiUrl: 'https://deviup.com.br:3003/api'
+  linguagensApiUrl: 'https://deviup.com.br:3003/api',
+  toastConfig: {
+    positionClass: 'toast-top-right',
+    preventDuplicates: true,
+    enableHtml: true,
+    progressBar: true,
+    timeOut:300
+  }
 };
 
 export const TOKEN_STORAGE = "NOME_CHAVE_TOKEN";
