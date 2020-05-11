198ec3228cfed0ae5b51d6891d0f6d5767906411
Cov Developer <shusten@hotmail.com>
2020-05-11 14:05:18 -0300

    :lipstick: style: formating the code

--- src/app/services/linguagem.service.ts
+++ src/app/services/linguagem.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 import { Linguagem } from '../interfaces/linguagem';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +14,9 @@ export class LinguagemService {
   private _linguagens: BehaviorSubject<Linguagem[]>;
   public readonly linguagens$: Observable<Linguagem[]>;
 
-  constructor(private http: HttpClient) { 
-  this._linguagens = new BehaviorSubject([]);
-  this.linguagens$ = this._linguagens.asObservable();
+  constructor(private http: HttpClient) {
+    this._linguagens = new BehaviorSubject([]);
+    this.linguagens$ = this._linguagens.asObservable();
   }
 
   atualizarLinguagens(): Observable<Linguagem[]> {
@@ -36,5 +36,5 @@ export class LinguagemService {
     const url = `${environment.linguagensApiUrl}/linguagens/curtir/${linguagem._id}`;
     return this.http.post(url, {});
   }
-  
+
 }
