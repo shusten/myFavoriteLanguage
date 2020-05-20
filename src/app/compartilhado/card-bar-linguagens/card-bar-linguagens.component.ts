c52aa9549ee3c36f1370a11f1505144b3d3c5d14
Luis Cov Jr <shusten@hotmail.com>
2020-05-19 22:35:17 -0300

    :construction: add bar chart in dashboard

--- src/app/compartilhado/card-bar-linguagens/card-bar-linguagens.component.ts
+++ src/app/compartilhado/card-bar-linguagens/card-bar-linguagens.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Linguagem } from 'src/app/interfaces/linguagem';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-card-bar-linguagens',
   templateUrl: './card-bar-linguagens.component.html',
   styleUrls: ['./card-bar-linguagens.component.css']
 })
-export class CardBarLinguagensComponent implements OnInit {
+export class CardBarLinguagensComponent {
+  public nomesLinguagens: string[];
+  public curtidasLinguagens: number[];
+  public tipoGrafico = 'bar';
+  alert: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set linguagens(linguagens: Linguagem[]) {
+    if (linguagens) {
+      this.nomesLinguagens = linguagens.map(linguagem => {
+        return linguagem.nome;
+      });
+      this.curtidasLinguagens = linguagens.map(linguagem => {
+        return linguagem.numeroUsuarios;
+      });
+    }
   }
-
 }
